35a97381b9de28cfba72f807b9d74e579c7cfbcb
moritz-burkert <76509604+moritz-burkert@users.noreply.github.com>
2026-09-24 16:16:54 +0200

    Tabelle zurücksetzen

--- Kunden.js
+++ Kunden.js
@@ -3,8 +3,6 @@ const fs = require("fs");
 const text = fs.readFileSync("Kunden_Dummy.csv", "utf8").replace(/^\uFEFF/, "");
 const [kopf, ...zeilen] = text.trim().split(/\r?\n/).map(z => z.split(";"));
 
-zeilen.forEach(z => {
-  console.log("-".repeat(40));
-  kopf.forEach((k, i) => console.log(k.padEnd(22) + z[i]));
-});
-console.log("-".repeat(40));
+const kunden = zeilen.map(z => Object.fromEntries(kopf.map((k, i) => [k, z[i]])));
+
+console.table(kunden);
