8a4c78a66eaf632aa87912d126989f9f454f2cb4
moritz-burkert <76509604+moritz-burkert@users.noreply.github.com>
2026-09-24 16:13:28 +0200

    Tabelle updaten

--- Kunden.js
+++ Kunden.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const text = fs.readFileSync("Kunden_Dummy.csv", "utf8").replace(/^\uFEFF/, "");
 const [kopf, ...zeilen] = text.trim().split(/\r?\n/).map(z => z.split(";"));
 
-const kunden = zeilen.map(z => Object.fromEntries(kopf.map((k, i) => [k, z[i]])));
-
-console.table(kunden);
+zeilen.forEach(z => {
+  console.log("-".repeat(40));
+  kopf.forEach((k, i) => console.log(k.padEnd(22) + z[i]));
+});
+console.log("-".repeat(40));
