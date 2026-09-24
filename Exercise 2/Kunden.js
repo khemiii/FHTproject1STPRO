cc9b8e5154ad8599af5668fc8b8a4d4fac12ead4
manigolamalipour-cell <mani.golamalipour@gmail.com>
2026-09-24 16:44:45 +0200

    Kommentar_commit

--- Exercise 2/Kunden.js
+++ Exercise 2/Kunden.js
@@ -5,4 +5,4 @@ const [kopf, ...zeilen] = text.trim().split(/\r?\n/).map(z => z.split(";"));
 
 const kunden = zeilen.map(z => Object.fromEntries(kopf.map((k, i) => [k, z[i]])));
 
-console.table(kunden);
+console.table(kunden); //bearbeitet von Mani
